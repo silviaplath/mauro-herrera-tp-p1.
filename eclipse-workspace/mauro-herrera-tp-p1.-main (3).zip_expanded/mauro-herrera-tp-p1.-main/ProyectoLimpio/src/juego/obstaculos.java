4f41a0428ca62b43d2104412efdde7e961cd9474
sele3001 <selenemauro3001@gmail.com>
2025-05-27 19:03:59 -0300

    cpñ

--- eclipse-workspace/mauro-herrera-tp-p1.-main (3).zip_expanded/mauro-herrera-tp-p1.-main/ProyectoLimpio/src/juego/obstaculos.java
+++ eclipse-workspace/mauro-herrera-tp-p1.-main (3).zip_expanded/mauro-herrera-tp-p1.-main/ProyectoLimpio/src/juego/obstaculos.java
@@ -8,17 +8,36 @@ import entorno.Herramientas;
 public class obstaculos {
 double x;
 double y;
+double altoroca;
+double anchoroca;
 Image imagen;
 
 
 public obstaculos (double x, double y) {
     this.x = x;
     this.y = y;
+    this.altoroca=50;
+    this.anchoroca=50;
     this.imagen = Herramientas.cargarImagen("imagenes/oie_transparent.png");
 }
 public void dibujar(Entorno entorno) {
-    entorno.dibujarImagen(this.imagen, this.x, this.y, 0, 0.05);
+	entorno.dibujarRectangulo(x, y, anchoroca, altoroca, 0.0, Color.LIGHT_GRAY);
+	entorno.dibujarImagen(this.imagen, this.x, this.y, 0, 0.05);
     
+}
+public double bordeInf() {
+	return this.y+altoroca/2;
+}
+public double bordeSup() {
+	return this.y-altoroca/2;
+	
+}
+public double bordeIzq() {
+	return this.x-anchoroca/2;
+	
+}
+public double bordeDer() {
+	return this.x+anchoroca/2;
 }
 
 }
